7696c7c894253a8fac972a10837ae62a2506c601
mato28 <matic.tempfer@gmail.com>
2019-05-19 11:10:54 +0200

    Create reports

--- src/views/Report.vue
+++ src/views/Report.vue
@@ -1,33 +1,104 @@
 <template>
   <div id="chart">
     <h1 class="subheading grey--text">Reports</h1>
-    <bar-chart :data="chartData"></bar-chart>
-    <line-chart :data="{'2017-05-13': 2, '2017-05-14': 5}"></line-chart>
-    <scatter-chart :data="[[174.0, 80.0], [176.5, 82.3]]" xtitle="Size" ytitle="Population"></scatter-chart>
-    <geo-chart :data="[['United States', 44], ['Germany', 23], ['Brazil', 22]]"></geo-chart>
+
+    <line-chart style="margin-top:30px" :data="dataForDays"></line-chart>
+    <bar-chart style="margin-top:30px" :data="chartData"></bar-chart>
+
+    <!-- <geo-chart :data="[['United States', 44], ['Germany', 23], ['Brazil', 22]]"></geo-chart> -->
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   components: {},
+  created() {
+    this.getReports();
+    this.getAds();
+  },
   data() {
     return {
+      reports: [],
+      ads: [],
+      adsNames: [],
       // Array will be automatically processed with visualization.arrayToDataTable function
-      chartData: [
-        ["Year", "Sales", "Expenses", "Profit"],
-        ["2014", 1000, 400, 200],
-        ["2015", 1170, 460, 250],
-        ["2016", 660, 1120, 300],
-        ["2017", 1030, 540, 350]
-      ],
+      chartData: [],
       chartOptions: {
         chart: {
-          title: "Company Performance",
-          subtitle: "Sales, Expenses, and Profit: 2014-2017"
+          title: "Ads Performance",
+          subtitle: "Reach: 2017-2019"
         }
-      }
+      },
+      dataForDays: {}
     };
+  },
+  methods: {
+    getReports() {
+      axios.get("http://193.2.178.254:3000/reports").then(response => {
+        //console.log(response + "ASasSasAS");
+        this.reports = response.data;
+        let adsReport = [];
+        for (let reportIndex in this.reports) {
+          const report = this.reports[reportIndex];
+
+          let index = -1;
+          for (let ad in adsReport) {
+            if (adsReport[ad].name === report.ad.name) {
+              index = ad;
+            }
+          }
+
+          let dates = { name: report.ad.name, data: {} };
+          if (index !== -1) {
+            dates = adsReport.splice(index, 1).pop();
+          }
+          console.log({ dates, index });
+
+          const current_datetime = new Date(report.createdAt);
+          let formatted_date =
+            current_datetime.getFullYear() +
+            "-" +
+            (current_datetime.getMonth() + 1) +
+            "-" +
+            current_datetime.getDate();
+          let data = dates["data"];
+          if (data.hasOwnProperty(formatted_date)) {
+            dates.data[formatted_date] += report.numberOfPeople;
+          } else {
+            dates.data[formatted_date] = 1;
+          }
+          adsReport = [...adsReport, dates];
+
+          console.log(adsReport);
+        }
+
+        let allTimeDataAd = [];
+        for (let adReport in adsReport) {
+          const ad = adsReport[adReport];
+          let name = ad.name;
+          let data = ad.data;
+          let allViews = 0;
+          for (let key in data) {
+            allViews += data[key];
+          }
+          allTimeDataAd = [...allTimeDataAd, [name, allViews]];
+        }
+        this.dataForDays = adsReport;
+        this.chartData = allTimeDataAd;
+      });
+    },
+    getAds() {
+      axios.get("http://193.2.178.254:3000/ads").then(response => {
+        //console.log(response + "ASasSasAS");
+        this.ads = response.data;
+
+        for (const ad in this.ads) {
+          console.log(this.ads[ad].name);
+          this.adsNames = [...this.adsNames, this.ads[ad].name];
+        }
+      });
+    }
   }
 };
 </script>
